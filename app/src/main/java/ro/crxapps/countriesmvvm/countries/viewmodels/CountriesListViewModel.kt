e0e487d7bb4a3fc52c085115dce1c29f438f385b
Rares Cruceat <cruceat.rares@gmail.com>
2020-04-10 12:10:33 +0200

    set mock countries in countries list view model and add icon and colors

--- app/src/main/java/ro/crxapps/countriesmvvm/countries/viewmodels/CountriesListViewModel.kt
+++ app/src/main/java/ro/crxapps/countriesmvvm/countries/viewmodels/CountriesListViewModel.kt
@@ -1,6 +1,38 @@
 package ro.crxapps.countriesmvvm.countries.viewmodels
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import ro.crxapps.countriesmvvm.countries.data.models.Country
 
 class CountriesListViewModel : ViewModel() {
+    val countries: MutableLiveData<List<Country>> = MutableLiveData()
+    val countryLoadError: MutableLiveData<Boolean> = MutableLiveData()
+    val loading: MutableLiveData<Boolean> = MutableLiveData()
+
+    fun refresh() {
+        fetchCountries()
+    }
+
+    private fun fetchCountries() {
+        val mockData: List<Country> = listOf(
+            Country("CountryA"),
+            Country("CountryB"),
+            Country("CountryC"),
+            Country("CountryD"),
+            Country("CountryE"),
+            Country("CountryF"),
+            Country("CountryG"),
+            Country("CountryH"),
+            Country("CountryI"),
+            Country("CountryJ"),
+            Country("CountryK"),
+            Country("CountryL"),
+            Country("CountryM"),
+            Country("CountryN")
+        )
+
+        countryLoadError.value = false
+        loading.value = false
+        countries.value = mockData
+    }
 }
